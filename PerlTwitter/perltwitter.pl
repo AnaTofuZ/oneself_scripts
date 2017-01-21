c0079af15cd65cfd4db3a2814c2594a3259ef77a
shimitakax <e155730@ie.u-ryukyu.ac.jp>
2017-01-21 16:49:23 +0900

    modified

--- PerlTwitter/perltwitter.pl
+++ PerlTwitter/perltwitter.pl
@@ -9,10 +9,6 @@ binmode STDOUT,":utf8";
 
 use Net::Twitter;
 
-my $consumerKey = "s8Z785x1q8OAgmhWmmfopA6vB";
-my $consumerSecret = "IGXvOnd6sKW7eAccoliiOiczZAKtzqyI6FKpfexNYJAaR7C9Zy";
-my $accessToken = "1014888828-21sBDgbmfnAi6gHv8CmXaT4ruIvM7u96ZZRL6Sx";
-my $accessTokenSecret = "OB9sO8oO1sY0tYVk9yVtiQmBlikbkkEXRhNU8qRZjNa1n";
 
 
 my $nt = Net::Twitter ->new({
