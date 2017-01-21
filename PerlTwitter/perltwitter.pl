05b9b756bacd36e839f7dcc1134fdfc26a5b40b5
shimitakax <e155730@ie.u-ryukyu.ac.jp>
2017-01-21 13:27:48 +0900

    add tweet

--- PerlTwitter/perltwitter.pl
+++ PerlTwitter/perltwitter.pl
@@ -2,15 +2,32 @@
 use strict;
 use warnings;
 use utf8;
+binmode STDOUT,":utf8";
 
 use Net::Twitter;
-use Scalar::Util 'blessed';
+
+my $consumerKey = "s8Z785x1q8OAgmhWmmfopA6vB";
+my $consumerSecret = "IGXvOnd6sKW7eAccoliiOiczZAKtzqyI6FKpfexNYJAaR7C9Zy";
+my $accessToken = "1014888828-21sBDgbmfnAi6gHv8CmXaT4ruIvM7u96ZZRL6Sx";
+my $accessTokenSecret = "OB9sO8oO1sY0tYVk9yVtiQmBlikbkkEXRhNU8qRZjNa1n";
 
 
-my $nt = Net::Twitter ->new(
-    traits => ['API::RESTv1_1'],
-    consumer_key => "",
-    consumer_secret => "",
-);
+my $nt = Net::Twitter ->new({
+    traits => [qw/OAuth API::RESTv1_1/],
+    consumer_key => $consumerKey,
+    consumer_secret => $consumerSecret,
+    access_token => $accessToken,
+    access_token_secret => $accessTokenSecret}
+    );
 
+while (<STDIN>) {
+    chomp;
+    &tweet($_);
+}
 
+sub tweet {
+    my ($msg) = @_;
+    my $ret = $nt->update({status=>$msg});
+    print "Cannot post!!" unless $ret;
+
+}
