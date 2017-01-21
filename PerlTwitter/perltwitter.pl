ab4644dba96011c3c23b67a22e3adf2e2c63bf96
shimitakax <e155730@ie.u-ryukyu.ac.jp>
2017-01-21 15:31:54 +0900

    add replay print

--- PerlTwitter/perltwitter.pl
+++ PerlTwitter/perltwitter.pl
@@ -3,6 +3,8 @@ use strict;
 use warnings;
 use utf8;
 use Encode;
+use Data::Dumper;
+
 binmode STDOUT,":utf8";
 
 use Net::Twitter;
@@ -21,8 +23,14 @@ my $nt = Net::Twitter ->new({
     access_token_secret => $accessTokenSecret}
     );
 
+
 while (<STDIN>) {
     chomp;
+    if ($_ eq "mention"){
+        &replay;
+        redo;
+    } 
+
     &tweet($_);
 }
 
@@ -31,5 +39,18 @@ sub tweet {
     $msg = decode_utf8($msg);
     my $ret = $nt->update({status=>$msg});
     print "Cannot post!!" unless $ret;
+}
 
+sub replay {
+
+    my $res = $nt->mentions();
+
+    for my $mention (@$res){
+        my $mentionText = $mention->{text};
+        my $mentionUser = $mention->{user}->{screen_name};
+        my $mentionID = $mention->{user}->{name};
+
+        printf(" %s 【%s】「%s」\n",$mentionID,$mentionUser,$mentionText);
+
+    }
 }
